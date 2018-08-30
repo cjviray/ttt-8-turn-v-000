b2e9af17f0c01ebada37643e8873787720323ee3
Christian V <viray.christianj@gmail.com>
2018-08-30 20:48:16 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,44 +1,44 @@
 def display_board(board)
-  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "#{board[6]} | #{board[7]} | #{board[8]}"
-end 
-
-def input_to_index(user_input)
-   user_input.to_i - 1
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move(board, index, value = "X")
-  board[index] = value
+def input_to_index(user_input) 
+  user_input.to_i - 1
 end
 
+def move(board, index, value="X")
+  board[index]=value
+end
+  
 def position_taken?(board, index)
-  if board[index] == "X" || board[index] == "O" 
+  if board[index] == "X" or board[index] == "O"
+return true
+else board[index] == " " or board[index] == "" or board[index] == nil
+return false
+end
+end
+
+ def valid_move?(board, index)
+  if index.between?(0, 8) && (position_taken?(board, index) == false)
     return true
-  else board[index] == " " || board[index] == "" || board[index] == nil
-    return false
-  end
-end
-
-def valid_move?(board, index)
-  if index.between?(0,8) && (position_taken?(board,index) == false)
-    true
-  else
+  else 
     return nil
-  end
+end
 end
 
-def turn(board)
+ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-    if valid_move?(board, index) == true
-      move(board, index, value = "X") &&
-      display_board(board)
-    else
-      turn(board)
-      end
+  
+  if valid_move?(board, index) == true
+	  move(board, index, value="X") &&
+	  display_board(board)
+  else
+  turn(board)
     end
-    
+end 
